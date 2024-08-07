ef586c7b1e791a311babac0572adbc2569f512f8
Dromen2001 <dromen2001@gmail.com>
2024-08-07 12:21:41 -0700

    buidling up performer and instructor forms

--- performer_u/main_app/urls.py
+++ performer_u/main_app/urls.py
@@ -5,7 +5,15 @@ urlpatterns = [
     path('/', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('instructors/', views.instructor_index, name='instructor-index'),
     path('performers/', views.performer_index, name='performer-index'),
-
+    path('performers/create', views.PerformerCreate.as_view(), name='performer-create'),
+    path('performers/<int:performer_id>/', views.performer_detail, name='performer-detail'),
+    path('performers/<int:pk>/update/', views.PerformerUpdate.as_view(), name='performer-update'),
+    path('performer/<int:pk>/delete/', veiws.PerformerDelete.as_view(), name='performer-delete'),
+    path('instructors/', views.instructor_list, name='instructor-list'),
+    path('instructors/create/', views.InstructorCreate.as_view(), name='instructor-create'),
+    path('instructors/<int:pk>/', views.InstructorDetail.as_view(), name='instructor-detail'),
+    path('instructors/<int:pk>/update/', views.InstructorUpdate.as_view(), name='instructor-update'),
+    path('instructors/<int:pk>/delete/', views.InstructorDelete.as_view(), name='instructor-delete'),
+    path('performers/<int:performer_id>/associate-instructor/<int:instructor_id>/', views.associate_instructor, name='associate-instructor'),
 ]
